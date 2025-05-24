4693c03c43a2c4f14f04c5741d1f353820c430ef
K.Geetha Uma Maheswari <143368865+geethauma815@users.noreply.github.com>
2025-05-24 21:21:46 +0530

    koko_eating_bananas.java
    1. first coco is given with an array of bananas and time like hours.
    2.  the possible it can eat in is in one hour it can eat 1 like that it eats starting from minimum 1 banana to maximum bananas in that array. 
    3. Now it should explore 1banana to max(bananas)
    4. uses binary search to get min banana that can eat to in the given time like hours
    for ex: 8hr like that it should in 8hrs,but with min bananas exactly.
    5. for calculating totalhours for each iteration of banana eating like 1 to max uses ceil value.
    
    7. like 2.33 means 3 like that 
    Box 1: 🍌🍌🍌
    Box 2: 🍌🍌🍌
    Box 3: 🍌        You still need a 3rd box for just 1 banana!

---
+++ BinarySearch/koko_eating_bananas.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1; // Minimum possible speed
+        int right = getMax(piles); // Maximum possible speed
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // Try middle speed
+            long hoursNeeded = getTotalHours(piles, mid);
+
+            if (hoursNeeded <= h) {
+                // Can finish in time or earlier -> try slower speed
+                right = mid - 1;
+            } else {
+                // Not enough time -> need to eat faster
+                left = mid + 1;
+            }
+        }
+
+        return left; // Minimum speed that works
+    }
+
+    // Calculating total hours needed at a given speed
+    static long getTotalHours(int[] piles, int speed) {
+        long totalHours = 0;
+        for (int i=0;i<piles.length;i++) {
+            // Use ceiling: (bananas + speed - 1) / speed
+            totalHours += (piles[i] + speed - 1) / speed;
+        }
+        return totalHours;
+    }
+
+    
+    static int getMax(int[] piles) {
+        int max = piles[0];
+        for (int i = 1; i < piles.length; i++) {
+            if (piles[i] > max) {
+                max = piles[i];
+            }
+        }
+        return max;
+    }
+}
